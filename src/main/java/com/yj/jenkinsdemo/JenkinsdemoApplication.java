797e0aaaba4acc63847f1e455cb79aa036e1dbed
yangjiaa <302831892@qq.com>
2020-08-22 17:56:00 +0800

    test1

--- src/main/java/com/yj/jenkinsdemo/JenkinsdemoApplication.java
+++ src/main/java/com/yj/jenkinsdemo/JenkinsdemoApplication.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@EnableConfigurationProperties
+
 @SpringBootApplication
 @EnableAutoConfiguration
 public class JenkinsdemoApplication {
